bca4dcb1812e3cdd8b2b062da9f249e488c0653d
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-08 17:14:44 +0800

    Task 1

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+name1 = input("What's your name?")
+name2 = input("What's your name?")
+name3 = input("What's your name?")
+
+print(f"Hello,{name1}")
+print(f"Hello,{name2}")
+print(f"Hello,{name3}")
