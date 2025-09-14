ded06ea2a3ffa280581b34bac391d323305bbf5f
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-14 15:23:41 +0800

    week1.1 Task2

---
+++ Week1.1 Task2.py
@@ -0,0 +1,16 @@
+RATE = 0.008
+
+try:
+    #get the money saved every month
+    money = int(input("Please enter the money you want to save every month:\n"))
+    total_money = money * 12
+    #Calculate the interest
+    interest = total_money * RATE
+    #Calculate the total money
+    final_money = total_money + interest
+
+    print(f"The amount saved every year:{total_money}")
+    print(f"The total money:{final_money}")
+
+except:
+    print("Invalid amount")
