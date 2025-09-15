3d144cfcca8484449b2b8ca215d478a49aa7e674
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-15 08:56:08 +0800

    Task3

--- classwork_week_02/week_02/session_1/tasks/task_3.py
+++ classwork_week_02/week_02/session_1/tasks/task_3.py
@@ -4,7 +4,12 @@ fruit = ("apple", "banana", "cherry")
 print(fruit)
 
 # Find and display position of "banana"
+print(fruit.index("banana"))
 
 # Display how many times "cherry" occurs
+print(fruit.count("cherry"))
 
 # Unpack tuple
+
+(first, second, third) = fruit
+print(first, second, third)
