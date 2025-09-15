a1e41189aac01b3edad53ece149f1b8abe48acc0
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-15 08:34:48 +0800

    Task2

--- classwork_week_02/week_02/session_1/tasks/task_2.py
+++ classwork_week_02/week_02/session_1/tasks/task_2.py
@@ -4,6 +4,15 @@ fruit = ["cherry", "strawberry", "melon", "grape", "apple"]
 
 # Sort list
 
+fruit.sort()
+print(fruit)
+
 # Reverse order of list items
 
+fruit.reverse()
+print(fruit)
+
 # Remove all items
+
+fruit.clear()
+print(fruit)
