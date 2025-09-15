737999d86dff018f620931037f3619c8daa23385
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-15 09:59:20 +0800

    Task6

--- classwork_week_02/week_02/session_1/tasks/task_6.py
+++ classwork_week_02/week_02/session_1/tasks/task_6.py
@@ -4,4 +4,16 @@ from pprint import pprint
 
 # Create music database, as a dictionary of strings mapped to lists
 
+list = {
+        "Radiohead": ["OK Computer", "KID A", "In Rainbows"],
+        "Kanye West": ["My beautiful Dark Twisted Fantasy", "The life of Pablo", "808 & Heartbreak"],
+        "The Weeknd": ["After Hours", "Dawn FM", "Hurry Up Tomorrow"]
+        }
+
+list.update({"Lorde":["Pure Heroine", "Melodrama", "Virgin"]})
+
 # Pretty-print the data structure
+#Contrast
+
+print(list)
+pprint(list)
