f936a6d15095def9b9f1db0c512def8061d4ca0c
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-20 13:52:44 +0800

    Week1.1

--- Week1/task2.py
+++ Week1/task2.py
@@ -2,7 +2,7 @@ RATE = 0.008
 
 try:
     #get the money saved every month
-    money = int(input("Please enter the money you want to save every month:\n"))
+    money = int(input("Please enter the money you want to save every month:"))
     total_money = money * 12
     #Calculate the interest
     interest = total_money * RATE
