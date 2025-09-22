a4aec256f92b841f3c1c52756e5c76dc0401af5a
Tsslarin <kdgq2045@leeds.ac.uk>
2025-09-22 10:28:58 +0800

    Week1.3

--- Week3/week-3-session-2/Task 2 - while-break/example1.py
+++ Week3/week-3-session-2/Task 2 - while-break/example1.py
@@ -3,7 +3,8 @@ k = 2
 while True :
     k = 2*k 
     print(k)
-    
+    if k>200:
+        break
 
 # As written this is an infinite loop.
 # If you run it, use <Ctrl>C to exit the running program.
@@ -11,4 +12,7 @@ while True :
 # 1. Add a break statement to exit the loop if k>200
 
 # 2. Rewrite the code to directly use the while loop instead of break.
-    
+a = 2
+while a<200:
+    a = 2 * a
+    print(a)
