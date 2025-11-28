ba9594d87b3e7970b52483bbc081a3bd0488a5a2
Tsslarin <kdgq2045@leeds.ac.uk>
2025-11-28 11:39:49 +0800

    Week11

--- Week11/week-10-session-1-worksheet/file_line_count.py
+++ Week11/week-10-session-1-worksheet/file_line_count.py
@@ -1,5 +1,5 @@
 
-# Name:
+# Name:Hangming Hu
 # Username: 
 
 import sys
@@ -8,9 +8,12 @@ import sys
 # Usage: python file_line_count.py <filename>
 # Example: python file_line_count.py textexample.txt
 # You must use the error messages precisely as defied below.
+if len(sys.argv)!=2:
+    print("Usage: python file_line_count.py <filename.txt>",file=sys.stderr)
+    sys.exit(1)
 
 # filename is a command line argument 
-file_name = ""
+file_name=sys.argv[1]
 
 # Error message: "Usage: python file_line_count.py <filename.txt>"
 
@@ -20,6 +23,15 @@ file_name = ""
 
 # count number of lines that are not blank
 number_of_lines = 0
+try:
+    with open(file_name, 'r') as f:
+        for line in f:
+            if line.strip():  # Check if the line is not empty
+                number_of_lines += 1
+    print(f"{file_name} has {number_of_lines} lines")
+except FileNotFoundError:
+    print(f"File not found: python file_line_count.py {file_name}", file=sys.stderr)
+    sys.exit(1)
 
 # success - report the number of lines
 
--- Week11/week-10-session-1-worksheet/ziplist.py
+++ Week11/week-10-session-1-worksheet/ziplist.py
@@ -11,9 +11,13 @@
 
 import sys
 #from zipfile import ...   # tools in this module can be used
+from zipfile import ZipFile, BadZipFile
 
 # filename is a command line argument 
-file_name = ""
+if len(sys.argv)!=2:
+    print("Usage: python ziplist.py <filename.zip>",file=sys.stderr)
+    sys.exit(1)
+file_name=sys.argv[1]
 
 # Error message: "Usage: python ziplist.py <filename.zip>"
 
@@ -21,6 +25,17 @@ file_name = ""
 
 # Error message: "Bad zip file: python ziplist.py {file_name}"
 # Hint: There is an exception for this defined in the zipfile module
+try:
+    with ZipFile(file_name, 'r') as zip_f:
+        file_list = zip_f.namelist()
+        for fname in file_list:
+            print(fname)
+except FileNotFoundError:
+    print(f"File not found: python ziplist.py {file_name}", file=sys.stderr)
+    sys.exit(1)
+except BadZipFile:
+    print(f"Bad zip file: python ziplist.py {file_name}", file=sys.stderr)
+    sys.exit(1)
 
 # Use zipfile methods to list the contents of the zip file.
 # Test your script on the zip_example.zip file. The response should be:
